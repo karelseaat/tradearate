6b884470b99107c9d84806e94c97ad5e3345da7b
Aat <karelseaat@gmail.com>
2021-10-05 11:55:46 +0200

    work on history-update to fix this cron issue

--- history-update.py
+++ history-update.py
@@ -4,25 +4,37 @@ from config import make_session
 from models import Trade, App, Review, Historic
 import logging
 import datetime
+import os
 
-logging.basicConfig(filename='history-update.log', level=logging.INFO)
+dirname = os.path.dirname(__file__)
+# print(dirname)
+
+logging.basicConfig(filename='{}/history-update.log'.format(dirname), level=logging.INFO)
 
 logging.info('Start of history update ' + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
 dbsession = make_session()
 
-tradenum = dbsession.query(Trade).count()
-appnum = dbsession.query(App).count()
-reviewnum = dbsession.query(Review).count()
+try:
+    tradenum = dbsession.query(Trade).count()
+    appnum = dbsession.query(App).count()
+    reviewnum = dbsession.query(Review).count()
+except error:
+   print("An error occured while counting objects", error)
 
-historyapp = Historic(infotype=0, number=appnum)
-historytrade = Historic(infotype=1, number=tradenum)
-historyreview = Historic(infotype=2, number=reviewnum)
+try:
+    historyapp = Historic(infotype=0, number=appnum)
+    historytrade = Historic(infotype=1, number=tradenum)
+    historyreview = Historic(infotype=2, number=reviewnum)
+except error:
+   print("An error occured where adding to history", error)
 
-
-dbsession.add(historytrade)
-dbsession.add(historyapp)
-dbsession.add(historyreview)
-dbsession.commit()
+try:
+    dbsession.add(historytrade)
+    dbsession.add(historyapp)
+    dbsession.add(historyreview)
+    dbsession.commit()
+except error:
+   print("An error occured while sending the whole shabang to the db", error)
 
 logging.info('End of history update ' + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
